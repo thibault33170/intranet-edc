cca1e6790735a274750d832df02b0ee70be3e0f2
thibault33170 <thibaultfaivre40@gmail.com>
2018-10-10 22:11:41 +0200

    Declare manually web routes

--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,77 @@ Route::group(['middleware' => 'auth'], function () {
     /**
      * Benevoles routes
      */
-    Route::resource('benevoles', 'BenevoleController');
+    Route::get('benevoles', 'BenevoleController@index')
+        ->name('benevoles.index');
+
+    Route::get('benevoles/create', 'BenevoleController@create')
+        ->name('benevoles.create');
+
+    Route::post('benevoles', 'BenevoleController@store')
+        ->name('benevoles.store');
+
+    Route::get('benevoles/{benevole}', 'BenevoleController@show')
+        ->name('benevoles.show');
+
+    Route::get('benevoles/{benevole}/edit', 'BenevoleController@edit')
+        ->name('benevoles.edit');
+
+    Route::put('benevoles/{benevole}', 'BenevoleController@update')
+        ->name('benevoles.update');
 
     /**
      * Capture routes
      */
-    Route::resource('captures', 'CaptureController');
-    Route::post('/captures/attach', 'CaptureController@attach')->name('captures.attach');
-    Route::post('/captures/detach', 'CaptureController@detach')->name('captures.detach');
+    Route::get('captures', 'CaptureController@index')
+        ->name('captures.index');
+
+    Route::get('captures/create', 'CaptureController@create')
+        ->name('captures.create');
+
+    Route::post('captures', 'CaptureController@store')
+        ->name('captures.store');
+
+    Route::get('captures/{capture}', 'CaptureController@show')
+        ->name('captures.show');
+
+    Route::get('captures/{capture}/edit', 'CaptureController@edit')
+        ->name('captures.edit');
+
+    Route::put('captures/{capture}', 'CaptureController@update')
+        ->name('captures.update');
+
+    Route::post('captures/attach', 'CaptureController@attach')
+        ->name('captures.attach');
+
+    Route::post('captures/detach', 'CaptureController@detach')
+        ->name('captures.detach');
 
     /**
      * Cats routes
      */
-    Route::resource('cats', 'CatController');
+    Route::get('cats', 'CatController@index')
+        ->name('cats.index');
+
+    Route::get('cats/create', 'CatController@create')
+        ->name('cats.create');
+
+    Route::post('cats', 'CatController@store')
+        ->name('cats.store');
+
+    Route::get('cats/{cat}', 'CatController@show')
+        ->name('cats.show');
+
+    Route::get('cats/{cat}/edit', 'CatController@edit')
+        ->name('cats.edit');
+
+    Route::put('cats/{cat}', 'CatController@update')
+        ->name('cats.update');
+
+    Route::post('cats/attach', 'CatController@attach')
+        ->name('cats.attach');
+
+    Route::post('cats/detach', 'CatController@detach')
+        ->name('cats.detach');
 });
 
 /**
